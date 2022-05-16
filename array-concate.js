a4738f10ae09a652a38402e385a2fdbf628a288b
Shivam Thorat <77840116+shivamthorat4@users.noreply.github.com>
2022-05-16 05:58:09 +0000

    Update array-concate.js

--- array-concate.js
+++ array-concate.js
@@ -1,4 +1,24 @@
+//Method 1
 let arr1 = [3,4,5];
 let arr2 = [6,7,8];
 
 console.log("Array after Concatination : "+ arr1.concat(arr2));
+
+//Method 2
+
+const arr3 = [10, 11, 12];
+const arr4 = [13,14,15];
+let run = 0, first = 0, second = 0;
+const newArr = [];
+while(run < arr3.length + arr4.length){
+   if(first > second){
+      newArr[run] = arr4[second];
+      second++;
+   }else{
+      newArr[run] = arr3[first];
+      first++;
+   }
+   run++;
+};
+console.log("------------------------------------------")
+console.log("Array after concatination : "+ newArr);
