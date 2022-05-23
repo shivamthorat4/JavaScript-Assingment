c4984f9d35567c651db1be9c78eb131713ee5735
Shivam Thorat <77840116+shivamthorat4@users.noreply.github.com>
2022-05-23 07:19:17 +0000

    Update function.js

--- function.js
+++ function.js
@@ -13,3 +13,59 @@ function double(x) { return x * 2; }
 let composedValue(square,double,5);
 console.log(composedValue(square,double,5));
 
+//composed value
+
+function composedValue(x){
+    var doubleresult=double(x);
+    var squareresult=square(doubleresult);
+    console.log(squareresult);
+}
+function square(x){
+    return x*x;
+}
+function double(x){
+    return x*2;
+}
+
+composedValue(3);
+
+//even
+
+function isEven(num){return (num%2==0);}
+
+function find(arr,isEven){
+    for(i=0;i<arr.length;i++){
+        if(isEven(arr[i])){
+            console.log(arr[i]);
+            break;
+        }
+    }
+}
+
+const arr=[1,3,5,4,2];
+console.log(isEven(3));//-->false
+console.log(isEven(4));//-->true
+find(arr,isEven);//-->4
+
+//map
+
+function square(x){return x*x;}
+function map(arr,square){
+    let outp=[];
+    for(i=0;i<arr.length;i++){
+        outp.push(square(arr[i]));
+    }
+    console.log(outp)
+}
+
+const arr=[1,2,3,4,5];
+map(arr,square);//-->[1,4,9,16,25]
+
+function map1(arr,Math){
+    let outp=[];
+    for(i=0;i<arr.length;i++){
+        outp.push(Math.sqrt(arr[i]));
+    }
+    console.log(outp)
+}
+map1([1,4,9,16,25],Math);//-->[1,2,3,4,5]
